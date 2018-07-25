5163774e51a7305538dbd2cf6415d823a1b79630
maxton <maxton@maxton.xyz>
2018-07-25 02:17:01 -0400

    Add PKG building to PkgTool

--- PkgTool/Program.cs
+++ PkgTool/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using LibOrbisPkg.GP4;
+using LibOrbisPkg.PFS;
+using LibOrbisPkg.PKG;
 
 namespace PkgTool
 {
@@ -21,10 +24,24 @@ namespace PkgTool
             {
               BlockSize = 65536,
               output = outFile,
-              proj = LibOrbisPkg.GP4.Gp4Project.ReadFrom(File.OpenRead(proj)),
+              proj = Gp4Project.ReadFrom(File.OpenRead(proj)),
               projDir = Path.GetDirectoryName(proj)
             };
-            new LibOrbisPkg.PFS.PfsBuilder().BuildPfs(props);
+            new PfsBuilder().BuildPfs(props);
+            break;
+          }
+        case "makepkg":
+          {
+            var proj = args[1];
+            var project = Gp4Project.ReadFrom(File.OpenRead(proj));
+            var outputPath = args[2];
+
+            using (var outFile = File.OpenWrite(Path.Combine(
+                outputPath,
+                $"{project.volume.Package.ContentId}.pkg")))
+            {
+              new PkgBuilder(project, Path.GetDirectoryName(proj)).Write(outFile);
+            }
             break;
           }
       }
