a45e0ecadfc85d8a2833fbbb2f62b8be5de6d866
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2019-06-14 16:12:40 +0100

    PkgEditor: seperate file open method

--- PkgEditor/MainWin.cs
+++ PkgEditor/MainWin.cs
@@ -73,22 +73,27 @@ namespace PkgEditor
       Application.Exit();
     }
 
+    private void openFile(string file)
+    {
+      switch (file.Split('.').Last().ToLowerInvariant())
+      {
+        case "gp4":
+          openGp4(file);
+          break;
+        case "pkg":
+          openPkg(file);
+          break;
+        case "sfo":
+          openSfo(file);
+          break;
+      }
+    }
+
     private void openToolStripMenuItem_Click(object sender, EventArgs e)
     {
       ShowOpenFileDialog("Open a GP4, PKG, SFO", "All Supported|*.gp4;*.pkg;*.sfo|GP4 Projects (*.gp4)|*.gp4|PKG Files (*.pkg)|*.pkg|SFO Files (*.sfo)|*.sfo", f =>
       {
-        switch (f.Split('.').Last().ToLowerInvariant())
-        {
-          case "gp4":
-            openGp4(f);
-            break;
-          case "pkg":
-            openPkg(f);
-            break;
-          case "sfo":
-            openSfo(f);
-            break;
-        }
+        openFile(f);
       });
     }
 
